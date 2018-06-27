773cf885ae83d8f7dc44215f10e6afdfec5f2733
Tanakun Wangpong <tanakun.wangpong@gmail.com>
2018-06-27 17:32:26 +0700

    fix uri mongo

--- config/env/local-development.js
+++ config/env/local-development.js
@@ -23,7 +23,8 @@ module.exports = {
     // uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://172.17.0.12:27019/magellen',
     // uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://localhost:27017/iot-v2',
     // uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://103.20.205.104:27019/magellen',
-    uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://103.20.204.176:27018/admin/myfarm',
+    // uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://103.20.204.176:27018/admin/myfarm',
+    uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://10.1.2.11:27018/admin/myfarm',
     // uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://localhost:27018/admin/myfarm',
     options: {
       user: 'admin',
